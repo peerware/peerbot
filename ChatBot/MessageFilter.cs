205c470ae172fe16aa7cf3c6fc34cbbf94931c3f
peer <ai.under.the.stars@gmail.com>
2022-07-07 10:54:15 -0400

    more antispam

--- ChatBot/MessageFilter.cs
+++ ChatBot/MessageFilter.cs
@@ -22,7 +22,7 @@ namespace ChatBot
         {
             message = message.ToLower();
 
-            if (message.Contains("bigfollows") || message.Contains("t to become famous") || username == "moobot" || message.Contains("primes and") || message.Contains("buy followers") || message.Contains("racis"))
+            if (message.Contains("bigfollows") || message.Contains("t to become famous") || username == "moobot" || message.Contains("primes and") || message.Contains("buy followers") || message.Contains("racis") || message.Contains("qualitу sеrvice") || message.Contains("rеal frее") || message.Contains("Тор streаmers"))
                 return true;
             else
                 return false;
