ea89d27d8d204937adbf3e1b32ce10779a30374a
phroggster <phroggster@users.noreply.github.com>
2022-07-08 04:41:03 -0500

    Add SteamWorks
    * This doesn't currently do anything
    * But, it does pave the way to having ThunderDome lobbies being
      announced in chat.
    * There's lots of things wrong in this currently, but I'm sharing this
      prematurely to allow for external input.

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using ChatBot.SteamWorks;
 
 namespace ChatBot
 {
     class Program
     {
+        private static void ReceivedLobbies(IEnumerable<ThunderDomeLobby> lobbies)
+        {
+            int count = lobbies.Count();
+            for (int n = 0; n < count; ++n)
+            {
+                var l = lobbies.ElementAt(n);
+                Console.Write($" Lobby {n}/{count} ({l.Id % 1000}): {l.NumPlayers}/{l.MaxMembers}, {l.AverageElo} skill");
+            }
+        }
+
         public static void Main()
         {
-            Console.ReadLine();
+            // Monitor thunderdome lobbies. Info will go to the console.
+            using (var cts = new CancellationTokenSource())
+            using (var tdwatcher = new LobbyWatcher(ReceivedLobbies,
+                l => l.AverageElo >= 100 && l.NumPlayers >= 1 && l.NumPlayers < l.MaxMembers && l.Status == ELobbyState.Queueing))
+            {
+                tdwatcher.BeginPolling(cts.Token);
+
+                Console.WriteLine("Press 'Q' to end program ..." + Environment.NewLine);
+                while (Console.ReadKey(true).Key != ConsoleKey.Q && tdwatcher.PollingTask.Status == System.Threading.Tasks.TaskStatus.Running)
+                {
+                    Thread.Sleep(200);
+                }
+                cts.Cancel();
+
+                if (!tdwatcher.PollingTask.Wait(10 * 1000))
+                {
+                    Console.WriteLine("took longer than 10 seconds to shutdown. naughty boi.");
+                }
+            }
         }
     }
 }
