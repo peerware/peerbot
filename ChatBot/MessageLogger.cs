912a738a6b321d28d87d971dfb289c49d789e205
peer <ai.under.the.stars@gmail.com>
2022-07-20 20:50:21 -0400

    Fixed bad code in MessageLogger

--- ChatBot/MessageLogger.cs
+++ ChatBot/MessageLogger.cs
@@ -14,23 +14,20 @@ namespace ChatBot
         
         public static void LogMessage(ChatMessage message)
         {
-            while (true)
+            lock (locker)
             {
-                lock (locker)
+                try
                 {
-                    try
-                    {
-                        // Get the current time in this time zone (I want the text file to also be in my timezone and this should account for daylight savings)
-                        TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-                        DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, easternZone);
+                    // Get the current time in this time zone (I want the text file to also be in my timezone and this should account for daylight savings)
+                    TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                    DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, easternZone);
 
-                        File.AppendAllText(filePath,  "\n" + easternTime.ToString("F") + " " + message.Username + ": " + message.Message);
-                        return;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.Write("Message logging failed.");
-                    }
+                    File.AppendAllText(filePath,  "\n" + easternTime.ToString("F") + " " + message.Username + ": " + message.Message);
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.Write("Message logging failed.");
                 }
             }
         }
