57cee14bf03131764889644a9a9c3da7aaf35959
James <ai.under.the.stars@gmail.com>
2024-10-14 15:30:46 -0400

    Middleware fix

--- YoutubeClient/Startup.cs
+++ YoutubeClient/Startup.cs
@@ -61,7 +61,6 @@ namespace YoutubeClient
         {
             try
             {
-                app.UseStaticFiles();
                 messageReceiver.twitchClient.OnMessageReceived += Client_OnMessageReceived;
 
                 app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -80,7 +79,7 @@ namespace YoutubeClient
                     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                     //app.UseHsts();
                 }
-                //app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
                 app.UseStaticFiles();
 
                 app.UseRouting();
